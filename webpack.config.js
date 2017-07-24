3ef1b555d6c3b8f5440f4772b96f28ebb41a0ab9
Derek Moon <dmoon79@gmail.com>
2017-07-24 13:25:50 -0700

    base working JS sandbox

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './dist',
+  },
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader','postcss-loader']
+        })
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [  {
+                    loader: 'css-loader',
+                    query: {
+                        modules: true,
+                        sourceMap: true,
+                        importLoaders: 2,
+                    }
+                  },
+                  'sass-loader'
+              ]
+        })
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      title: 'JS Sandbox'
+    }),
+    new ExtractTextPlugin('style.css')
+  ]
+}
