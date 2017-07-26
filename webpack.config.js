e9c1f4a298d9f87917932933abb2719b4e0b24fb
Derek Moon <dmoon79@gmail.com>
2017-07-26 10:21:27 -0700

    add readme

--- webpack.config.js
+++ webpack.config.js
@@ -26,11 +26,6 @@ module.exports = {
         test: /\.(js|jsx)$/,
       },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
         exclude: /node_modules/,
         test: /\.html$/,
         use: 'html-loader'
